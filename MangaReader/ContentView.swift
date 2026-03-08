92bcb7b32d5be6fc38006df039f81c5ea31a7fff
LiWinDom <wadim.gorbachev@mail.ru>
2026-03-08 15:59:26 +0300

    Initial Commit

---
+++ MangaReader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MangaReader
+//
+//  Created by LiWinDom on 08.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MangaReader/MangaReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangaReaderApp.swift
+//  MangaReader
+//
+//  Created by LiWinDom on 08.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct MangaReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
